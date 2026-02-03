845528149d1eedf2b7710f12fd959a047125e4b7
Aleksa <office.aleksa.milanovic@gmail.com>
2026-02-04 00:33:59 +0100

    Working

--- modules/register.php
+++ modules/register.php
@@ -1,5 +1,17 @@
-<?php?>
+<?php
+
+
+
+?>
 
 <div class="register">
     <h2>Register</h2>
+
+    <form method="POST">
+        <input type="text" placeholder="Username" name="username">
+        <input type="text" placeholder="Phone number" name="phone_number">
+        <input type="password" placeholder="Password" name="password">
+        <input type="password" placeholder="Confirm password" name="password-confirm">
+        <button type="submit">Register</button>
+    </form>
 </div>
