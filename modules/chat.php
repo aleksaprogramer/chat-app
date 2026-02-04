7b63ef72161062a6a606a59cd5c3dd2b5d87627b
Aleksa <office.aleksa.milanovic@gmail.com>
2026-02-04 21:01:40 +0100

    Getting users

--- modules/chat.php
+++ modules/chat.php
@@ -1,5 +1,47 @@
-<?php?>
+<?php
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: " . $env->base_url . "?router=login");
+    exit();
+}
+
+if (!isset($_GET['id'])) {
+    header("Location: " . $env->base_url . "?router=homepage");
+    exit();
+}
+
+if ($_SESSION['user_id'] === $_GET['id']) {
+    header("Location: " . $env->base_url . "?router=homepage");
+    exit();
+}
+
+// Getting user one
+$user_one_id = $_SESSION['user_id'];
+$sql = "SELECT username FROM users WHERE id = $user_one_id";
+$run = $db->query($sql);
+$user_one = $run->fetch_assoc();
+
+// Getting user two
+$user_two_id = mysqli_real_escape_string($db, $_GET['id']);
+$sql = "SELECT username FROM users WHERE id = $user_two_id";
+$run = $db->query($sql);
+$user_two = $run->fetch_assoc();
+
+if (!$user_two) {
+    header("Location: " . $env->base_url . "?router=homepage");
+    exit();
+}
+
+?>
 
 <div class="chat">
     <h2>Chat</h2>
+
+    <form>
+        <input type="hidden" id="username" value="<?php echo $user_one['username'] ?>">
+        <input type="text" placeholder="Message" id="message">
+        <button type="submit">Send</button>
+    </form>
+
+    <div class="messages-output"></div>
 </div>
